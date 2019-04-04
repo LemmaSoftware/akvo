02a840118a3e8ddff525920488a15cd87665781d
Trevor Irons <Trevor.Irons@lemmasoftware.org>
2019-04-04 10:23:02 -0600

    1.0.13 update to GUI for formatting

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='Akvo',
-      version='1.0.12',
+      version='1.0.13',
       description='Surface nuclear magnetic resonance workbench',
       long_description=long_description,
       author='Trevor P. Irons',
