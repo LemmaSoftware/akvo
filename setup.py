57381b7c4f8d48ad9fab874f91516e2be323c287
Trevor Irons <Trevor.Irons@lemmasoftware.org>
2019-06-03 09:59:48 -0600

    1.2.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='Akvo',
-      version='1.2.1',
+      version='1.2.2',
       description='Surface nuclear magnetic resonance workbench',
       long_description=long_description,
       long_description_content_type='text/markdown',
