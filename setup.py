7d80e34256886a530624a266b688eb88336f2ce0
Trevor Irons <Trevor.Irons@lemmasoftware.org>
2021-07-11 15:43:13 -0600

    update of setuptools for multicomputing

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='Akvo',     
-      version='1.5.2', 
+      version='1.6.0', 
       python_requires='>3.7.0', # due to pyLemma 
       description='Surface nuclear magnetic resonance workbench',
       long_description=long_description,
@@ -53,6 +53,8 @@ setup(name='Akvo',
           'pandas',
           'pyqt-distutils',
           'cmocean',
+          'multiprocessing',
+          'itertools',
           'pyLemma >= 0.4.0'
       ],
       packages=['akvo', 'akvo.tressel', 'akvo.gui'],
