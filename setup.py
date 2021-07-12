476f2a3964392cd8e0a5910cef080f3bd6e485b9
Trevor Irons <Trevor.Irons@lemmasoftware.org>
2021-07-12 12:26:33 -0600

    1.6.2 itertools is built in, removed from install requirements

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='Akvo',     
-      version='1.6.1', 
+      version='1.6.2', 
       python_requires='>3.7.0', # due to pyLemma 
       description='Surface nuclear magnetic resonance workbench',
       long_description=long_description,
@@ -54,7 +54,6 @@ setup(name='Akvo',
           'pyqt-distutils',
           'cmocean',
           'multiprocessing',
-          'itertools',
           'pyLemma >= 0.4.0'
       ],
       packages=['akvo', 'akvo.tressel', 'akvo.gui'],
